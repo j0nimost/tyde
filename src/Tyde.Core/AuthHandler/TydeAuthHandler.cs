c21a2b5b437b866ad4ae89a4c0c3dbc296b0d8d1
John Nyingi <john.nyingi@live.com>
2022-02-24 11:46:51 +0300

    Added new AuthHandeler

---
+++ src/Tyde.Core/AuthHandler/TydeAuthHandler.cs
@@ -0,0 +1,18 @@
+﻿using Tyde.Shared.Configurations;
+
+
+namespace Tyde.Core.AuthHandler
+{
+    public class TydeAuthHandler
+    {
+        private readonly HttpClient? _httpClient;
+        private readonly ITydeExtensionFactory _extensionFactory;
+
+        public TydeAuthHandler(ITydeExtensionFactory tydeExtensionFactory)
+        {
+            _extensionFactory = tydeExtensionFactory;
+        }
+
+        
+    }
+}
