3648c49c5dde9da4284f2e83397c3505e9308888
j0nimost <john.nyingi@live.com>
2022-03-02 14:09:02 +0300

    rm unnecessary configs and added only the most necessary

--- src/Tyde.Shared/Configurations/TydeConfiguration.cs
+++ src/Tyde.Shared/Configurations/TydeConfiguration.cs
@@ -9,20 +9,28 @@ namespace Tyde.Shared.Configurations
     public class TydeConfiguration
     {
         /// <summary>
-        /// Token TagName i.e "token": "EQ4ahSESaDFE".
+        /// Token TagName i.e "token": "EQ4ahSESaDFE". <br/>
         /// Default value is "token"
         /// </summary>
-        public static string TagName { get; set; } = "token"; // default 
+        public static string TokenTagName { get; set; } = "token"; // default 
+        /// <summary>
+        /// ExpiresIn TagName i.e "expiresIn": 1800 <br/>
+        /// This is the json response timespan from the Authorization API <br/>
+        /// Default value is "expiresIn"
+        /// </summary>
+        public static string ExpiresInTagName { get; set; } = "expiresIn";
+        /// <summary>
+        /// Any additional Headers needed for any other subsequent request <br/> <br/> example;
+        /// "CLIENTID": "er3rfdqe8ADSH" <br/>
+        /// You can pass empty values for the values to be picked from Authorization API Response, <br/>
+        /// else the passed value will be used as they are.
+        /// </summary>
+        public static Dictionary<string, string>? AdditionalHeaders { get; set; }
         /// <summary>
         /// Expected TimeSpan to expire.
-        /// Default value is 1hr
+        /// Default value is Zero
         /// </summary>
-        public static TimeSpan Expires_In { get; set; } = TimeSpan.FromSeconds(3600);
-        /// <summary>
-        /// Expected DateTimeOffset of to time to expire.
-        /// Optional, if not provided package will use <see cref="Expires_In"/>
-        /// </summary>
-        public static DateTimeOffset? Expires_At { get; set; }
+        public static TimeSpan Expires_In { get; set; } = TimeSpan.Zero;
         /// <summary>
         /// The Authentication Url, where the service retrives the tokens
         /// </summary>
