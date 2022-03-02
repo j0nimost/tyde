ce80dc4a467d80258e321d606add9fd63ce251b7
j0nimost <john.nyingi@live.com>
2022-03-02 14:07:01 +0300

    Added an append method to allow users to add own custom headers

--- src/Tyde.Shared/Configurations/TydeExtensionFactory.cs
+++ src/Tyde.Shared/Configurations/TydeExtensionFactory.cs
@@ -6,7 +6,7 @@ namespace Tyde.Shared.Configurations
     public class TydeExtensionFactory : ITydeExtensionFactory
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly Dictionary<string, string>? _SerializationConfig;
+        private readonly Dictionary<string, string> _SerializationConfig;
 
         private HttpClient? _httpClient;
 
@@ -21,10 +21,26 @@ namespace Tyde.Shared.Configurations
 
             _SerializationConfig = new Dictionary<string, string>
             {
-                {TydeConfiguration.TagName, "" } // allows us to deserialize irregardless of the value passed
+                {TydeConfiguration.TokenTagName, "" }, // allows us to deserialize irregardless of the value passed
+                {TydeConfiguration.ExpiresInTagName, "" },
+                
+
             };
 
+            AppendAdditionalHeaders();
         }
+
+        private void AppendAdditionalHeaders()
+        {
+            if (TydeConfiguration.AdditionalHeaders == null)
+                return;
+
+            foreach (KeyValuePair<string, string> items in TydeConfiguration.AdditionalHeaders)
+            {
+                _SerializationConfig.Add(items.Key, items.Value);
+            }
+        }
+
 
         public HttpClient HttpClientInstance {
            get
@@ -35,7 +51,7 @@ namespace Tyde.Shared.Configurations
                     var httpInstance = _serviceProvider.GetRequiredService<HttpClient>();
 
                     if(httpInstance == null)
-                        throw new ArgumentNullException(nameof(_httpClient));
+                        throw new ArgumentNullException(nameof(HttpClient));
 
                     _httpClient = httpInstance;
                 }
@@ -43,6 +59,11 @@ namespace Tyde.Shared.Configurations
             }
          }
 
+        /// <summary>
+        /// Contains all the parameters needed to process the Authorization JSON response,
+        /// Default values will not be overwritten. <br/>
+        /// Token and Expires_In will always overwritten when session expires.
+        /// </summary>
         public Dictionary<string, string> SerializationConfig => _SerializationConfig ?? throw new ArgumentNullException(nameof(SerializationConfig));
     }
 }
