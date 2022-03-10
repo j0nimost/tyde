5dd33f930bf625f0dcb535b6a88bcb4ed3e97c9f
j0nimost <john.nyingi@live.com>
2022-03-10 15:03:06 +0300

    Added new feature to handler Auth

--- src/Tyde.Core/AuthHandler/TydeAuthHandler.cs
+++ src/Tyde.Core/AuthHandler/TydeAuthHandler.cs
@@ -3,6 +3,8 @@ using System.Text.Json;
 using System.Text;
 using Tyde.Core.Cache;
 using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Runtime.Remoting;
 
 namespace Tyde.Core.AuthHandler
 {
@@ -12,10 +14,12 @@ namespace Tyde.Core.AuthHandler
         private readonly ITydeExtensionFactory _extensionFactory;
         private readonly ITydeCache _tydeCache;
         private readonly TydeConfiguration _tydeConfiguration;
+        private readonly TydeJsonOptions _tydeJsonOptions;
 
-        public TydeAuthHandler(ITydeExtensionFactory tydeExtensionFactory, ITydeCache tydeCache, IOptions<TydeConfiguration> tydeConfiguration)
+        public TydeAuthHandler(ITydeExtensionFactory tydeExtensionFactory, ITydeCache tydeCache, IOptions<TydeConfiguration> tydeConfiguration, IOptions<TydeJsonOptions> jsonOptions)
         {
             _tydeConfiguration = tydeConfiguration.Value;
+            _tydeJsonOptions = jsonOptions.Value;
             _extensionFactory = tydeExtensionFactory;
             _tydeCache = tydeCache;
             _httpClient = _extensionFactory.HttpClientInstance ?? throw new ArgumentNullException("httpclient instance from IServices is Null");
@@ -35,17 +39,17 @@ namespace Tyde.Core.AuthHandler
                 var response = await _httpClient.PostAsync(_tydeConfiguration.AuthenticationUrl, httpContent);
 
                 response.EnsureSuccessStatusCode();
-
+                
                 string responseBody = await response.Content.ReadAsStringAsync();
 
-                Dictionary<string, string>? responseDict = JsonSerializer.Deserialize<Dictionary<string, string>>(responseBody);
+                Dictionary<string, object> responseObj = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody, _tydeJsonOptions.JsonSerializerOptions);
 
-                if(responseDict == null)
-                {
-                    throw new NullReferenceException("Authorizing API returned an empty response or an incorrect respose");
-                }
 
-                ResponseCacheAppend(responseDict);
+                if (responseObj == null)
+                    throw new Exception("Incorrect deserialization of the response,\n either incorrect object was returned or an empty response");
+
+
+                ResponseCacheAppend(responseObj);
             }
             catch (Exception)
             {
@@ -55,31 +59,31 @@ namespace Tyde.Core.AuthHandler
         }
 
 
-        private void ResponseCacheAppend(Dictionary<string, string> responseDict)
+        private void ResponseCacheAppend(Dictionary<string, object> responseDict)
         {
-            foreach(KeyValuePair<string, string> res in responseDict)
+            foreach(KeyValuePair<string, object> res in responseDict)
             {
                 if(_extensionFactory.SerializationConfig.ContainsKey(res.Key))
                 {
-                    if(String.IsNullOrEmpty(res.Value))
+                    if(res.Value == null)
                         throw new ArgumentNullException($"{nameof(res.Key)} From Authorization API has an empty value");
 
 
                     if(_tydeConfiguration.ExpiresInTagName.Equals(res.Key))
                     {
-                        TimeSpan expiresIn = TimeSpan.Parse(res.Value);
+                        TimeSpan expiresIn = (TimeSpan)res.Value;
                         _tydeCache.SetExpiresAt(expiresIn); // update only when Expires_In is not set
                         continue;
                     }
 
                     if(String.IsNullOrEmpty(_extensionFactory.SerializationConfig[res.Key]) || _tydeConfiguration.TokenTagName == res.Key)
-                        _tydeCache.AddSessionToken(res.Key, res.Value); // append the respective key
+                        _tydeCache.AddSessionToken(res.Key, Convert.ToString(res.Value)); // append the respective key
                     else
                         _tydeCache.AddSessionToken(res.Key, _extensionFactory.SerializationConfig[res.Key]); // if the customer has defined own values skip updating the value
                 }
-
             }
-
         }
+
+
     }
 }
